7d7c428fa65e3b9ee7d5670d614e6bdb4d7e6c82
mattdvhope <mattagape@me.com>
2020-05-20 12:40:45 +0700

    Test code

--- examples/echobot/app.rb
+++ examples/echobot/app.rb
@@ -1,36 +1,40 @@
 require 'sinatra'   # gem 'sinatra'
 require 'line/bot'  # gem 'line-bot-api'
 
-def client
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
+get '/' do
+  "Hello World"
 end
 
-post '/callback' do
-  body = request.body.read
+# def client
+#   @client ||= Line::Bot::Client.new { |config|
+#     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+#     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+#   }
+# end
 
-  signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless client.validate_signature(body, signature)
-    halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
-  end
+# post '/callback' do
+#   body = request.body.read
 
-  events = client.parse_events_from(body)
+#   signature = request.env['HTTP_X_LINE_SIGNATURE']
+#   unless client.validate_signature(body, signature)
+#     halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
+#   end
 
-  events.each do |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], message)
-      end
-    end
-  end
+#   events = client.parse_events_from(body)
 
-  "OK"
-end
+#   events.each do |event|
+#     case event
+#     when Line::Bot::Event::Message
+#       case event.type
+#       when Line::Bot::Event::MessageType::Text
+#         message = {
+#           type: 'text',
+#           text: event.message['text']
+#         }
+#         client.reply_message(event['replyToken'], message)
+#       end
+#     end
+#   end
+
+#   "OK"
+# end
