5351d54f42185800872ba6d6784791bdca3ca293
mattdvhope <mattagape@me.com>
2020-05-21 12:12:35 +0700

    Testing post

--- examples/echobot/app.rb
+++ examples/echobot/app.rb
@@ -3,8 +3,8 @@ require 'line/bot'  # gem 'line-bot-api'
 
 def client
   @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    config.channel_secret = "04ad4901f521f3f8c041b3240765c409" # ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = "/pLbQN3KXuTyg9N1fr2uzheTnP9OiVBWLd6GlBgH4V//zHmtAFSK0Y293Fb9wKO4W1oI0b4ueFeYV64wGSvo3oC0Zxrs2B1ZuuAyqEZ7Go2u2zyLkfcT2SP5dpA4Y9nllkyxwDGjq13dptsAXLQ/CQdB04t89/1O/w1cDnyilFU=" # ENV["LINE_CHANNEL_TOKEN"]
   }
 end
 
@@ -12,6 +12,17 @@ post '/callback' do
   body = request.body.read
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
+
+  CHANNEL_SECRET = "04ad4901f521f3f8c041b3240765c409"    # ENV["LINE_CHANNEL_SECRET"] # Channel secret string
+  http_request_body = request.raw_post # Request body string
+  hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, CHANNEL_SECRET, http_request_body)
+  signature_checked = Base64.strict_encode64(hash)
+
+  # signature_verified = signature==signature_checked
+
+  p "signature: " + signature
+  p "signature_checked: " + signature_checked
+
   unless client.validate_signature(body, signature)
     halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
   end
